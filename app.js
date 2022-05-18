058b4d54e134a7eed3cc4183284a2349150e0ecd
SankaviS <ssangavi14@gmail.com>
2022-05-18 20:24:06 +0530

    My first commit

--- app.js
+++ app.js
@@ -5,15 +5,18 @@ import router from "././routes/user-routes ";
 import blogRouter from "./routes/blog-routes";
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use("/user", router);
 app.use("/blog", blogRouter);
-app.use(cors());
-let port = process.env.PORT || 3000;
+
+let port = process.env.PORT || 5000;
+var url =
+  process.env.MONGODB_URI ||
+  "mongodb+srv://sankavi:sankavi23@cluster0.hx5vd.mongodb.net/CMS?retryWrites=true&w=majority";
+
 mongoose
-  .connect(
-    "mongodb+srv://sankavi:sankavi23@cluster0.hx5vd.mongodb.net/CMS?retryWrites=true&w=majority"
-  )
+  .connect(url)
   .then(() => {
     app.listen(port);
   })
